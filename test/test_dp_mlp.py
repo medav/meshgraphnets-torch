b01f23ce4c12408c0b941f23d0f20f47a531752b
Michael Davies <davies@cs.wisc.edu>
2023-03-02 14:05:19 -0600

    update test_dp_mlp

--- test/test_dp_mlp.py
+++ test/test_dp_mlp.py
@@ -2,67 +2,54 @@ import sys
 sys.path.append('.')
 
 import time
-import pypin
 import torch
-from torch.profiler import profile, record_function, ProfilerActivity
 import graphnet as GNN
 
-NUM_ITERS = 10
-BATCH_SIZE = 32
+BATCH_SIZE = int(sys.argv[1])
+NUM_ITERS = 2048 * 64 // BATCH_SIZE
 
-D = 128
+NN = 1226
+NE = 12281
+NW = 1660
 
+D = int(sys.argv[2])
 
 class PsuedoGraphNetBlock(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.node_mlp  = GNN.Mlp(3 * D, [D, D, D])
-        self.edge_mlp  = GNN.Mlp(3 * D, [D, D, D])
-        self.world_mlp = GNN.Mlp(3 * D, [D, D, D])
+        self.node_mlp  = GNN.Mlp(3 * D, [D, D, D], layernorm=False)
+        self.edge_mlp  = GNN.Mlp(3 * D, [D, D, D], layernorm=False)
+        self.world_mlp = GNN.Mlp(3 * D, [D, D, D], layernorm=False)
 
     def forward(self, n, e, w):
-        n = self.node_mlp(torch.cat([n, n, n], dim=1))
-        e = self.edge_mlp(torch.cat([e, e, e], dim=1))
-        w = self.world_mlp(torch.cat([w, w, w], dim=1))
-        return n, e, w
-
-class PsuedoGraphNet(torch.nn.Module):
-    def __init__(self, num_mp_steps=15):
-        super().__init__()
-        self.num_mp_steps = num_mp_steps
-        self.blocks = torch.nn.ModuleList([
-            PsuedoGraphNetBlock() for _ in range(num_mp_steps)])
-
-    def forward(self, n, e, w):
-        for block in self.blocks:
-            n, e, w = block(n, e, w)
+        n = self.node_mlp(n)
+        e = self.edge_mlp(e)
+        w = self.world_mlp(w)
         return n, e, w
 
 
 dev = torch.device('cuda:0')
-net = PsuedoGraphNet().half().to(dev)
-n = torch.randn(BATCH_SIZE * 1226,  D, device=dev, dtype=torch.float16)
-e = torch.randn(BATCH_SIZE * 12281, D, device=dev, dtype=torch.float16)
-w = torch.randn(BATCH_SIZE * 1660,  D, device=dev, dtype=torch.float16)
+is_cuda = dev.type == 'cuda'
+dtype = torch.float16
+net = PsuedoGraphNetBlock().to(dtype).to(dev)
 
-# with pypin.pinhooks.perf_roi(0, 'mlp', ''):
+flops = 10 * D * D * (NN + NE + NW)
 
-with profile(
-    activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
-    with_stack=True,
-    profile_memory=True,
-) as prof:
-    t0 = time.perf_counter()
-    for i in range(NUM_ITERS):
-        net(n, e, w)
-        prof.step()
-    t1 = time.perf_counter()
+n1 = torch.randn(BATCH_SIZE * NN, 3 * D, device=dev, dtype=dtype)
+e1 = torch.randn(BATCH_SIZE * NE, 3 * D, device=dev, dtype=dtype)
+w1 = torch.randn(BATCH_SIZE * NW, 3 * D, device=dev, dtype=dtype)
+
+
+t0 = time.perf_counter()
+for i in range(NUM_ITERS):
+    net(n1, e1, w1)
+
+if is_cuda: torch.cuda.synchronize()
+t1 = time.perf_counter()
 
 print(f'Batch Size: {BATCH_SIZE}')
 print(f'Num Iters: {NUM_ITERS}')
 print(f'Elapsed time: {t1 - t0:.2f} seconds')
 print(f'Throughput: {NUM_ITERS * BATCH_SIZE / (t1 - t0):.2f} samp/sec')
+print(f'Compute: {flops * NUM_ITERS * BATCH_SIZE / (t1 - t0) / 1e9:.2f} GFLOPS')
 
-# print(prof \
-#     .key_averages(group_by_input_shape=False, group_by_stack_n=4) \
-#     .table(sort_by="self_cuda_time_total", row_limit=-1, top_level_events_only=False))
